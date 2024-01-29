d0cfe2038cc20c2b16645f3ed8de6f07a9013463
AlinaB <bernardezalina@gmail.com>
2024-01-29 15:49:41 +0100

    ejercicio

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3002;
+
+app.use(cors())
+
+const mainURL = 'https://rickandmortyapi.com/api/character'
+
+app.get('/characters', async (req, res) => {
+    try {
+        const response = await axios.get(mainURL)
+        res.json(response.data.results)
+    } catch(error) {
+        res.status(404).json({message: 'No response!'})
+    }
+});
+
+app.get('/characters/:name', async (req, res) => {
+    const name = req.params.name
+    const url = `${mainURL}/?name=${name}`
+    try {
+        const response = await axios.get(url)
+        res.json(response.data.results)
+    } catch(error) {
+        res.status(404).json({message: 'No character found!'})
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend listening http://localhost:${PORT}`)
+})
